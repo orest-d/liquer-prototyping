3a9db1fd18c198e2812f1345ee6e3d58f52372b4
Orest Dubay <orest3.dubay@gmail.com>
2023-01-30 23:41:38 +0100

    Plan in progress

--- rsliquer2/src/plan.rs
+++ rsliquer2/src/plan.rs
@@ -1,9 +1,16 @@
-use crate::query::{Query, ActionRequest};
+use crate::query::{Query, ActionRequest, ResourceName, QuerySegment};
 
 pub enum Step{
     Get(String),
+    GetRes(Vec<ResourceName>),
     GetMeta(String),
-    Evaluate(ActionRequest)
+    GetResMeta(Vec<ResourceName>),
+    Evaluate(Query),
+    ApplyAction(ActionRequest),
+    Filename(ResourceName),
+    Info(String),
+    Warning(String),
+    Error(String)
 }
 
 
@@ -11,6 +18,83 @@ pub struct Plan{
     query:Query,
     steps:Vec<Step>
 }
+
+impl Plan {
+    fn from(query:&Query) -> Plan {
+        let mut plan = Plan{query:query.clone(), steps:vec![]};        
+        let (p,r)=query.predecessor();
+        match r{
+            Some(QuerySegment::Resource(res)) => {
+                if let Some(p) = p{
+                    if !p.is_empty() {
+                        plan.warning(format!("Query '{}' before resource at {} is ignored", p.encode(), res.position()));
+                    }
+                }
+                if let Some(head) = res.header{
+                    if !head.name.is_empty() {
+                        plan.warning(format!("Resource segment name '{}' at {} is ignored", head.name, head.position));
+                    }
+                    if head.parameters.is_empty(){
+                        plan.steps.push(Step::GetRes(res.query.clone()));
+                    }
+                    else{
+                        if head.parameters[0].value == "meta"{
+                            if head.parameters.len()>1{
+                                plan.warning(format!("Resource segment '{}...' parameters after meta at {} ignored", head.encode(), head.parameters[2]));
+                            }
+                            plan.steps.push(Step::GetResMeta(res.query.clone()));
+                        }
+                        else {
+                            plan.warning(format!("Resource segment '{}...' parameters at {} ignored", head.encode(), head.parameters[0]));
+                            plan.steps.push(Step::GetRes(res.query.clone()));
+                        }
+                    }
+                }
+                else{
+                    plan.steps.push(Step::GetRes(res.query.clone()));
+                }
+            },
+            Some(QuerySegment::Transform(tqs)) => {
+                if let Some(p) = p{
+                    if !p.is_empty() {
+                        plan.steps.push(Step::Evaluate(p.clone()));
+                    }
+                }
+                if let Some(action) = tqs.action(){
+                    plan.steps.push(Step::ApplyAction(action.clone()));   
+                }
+                else{
+                    if tqs.is_filename(){
+                        plan.steps.push(Step::Filename(tqs.filename.unwrap().clone()));
+                    }
+                    else{
+                        plan.error(format!("Unrecognized remainder {:?}",tqs));
+                    }
+                }
+
+            }
+            None => {
+                if let Some(p) = p{
+                    if !p.is_empty() {
+                        plan.steps.push(Step::Evaluate(p.clone()));
+                    }
+                }
+                plan.warning(format!("Empty remainder"));
+            }
+        }
+        plan
+    }
+    fn info(&mut self, message:String){
+        self.steps.push(Step::Info(message));
+    }
+    fn warning(&mut self, message:String){
+        self.steps.push(Step::Warning(message));
+    }
+    fn error(&mut self, message:String){
+        self.steps.push(Step::Error(message));
+    }
+}
+
 #[cfg(test)]
 mod tests{
     use super::*;
@@ -18,5 +102,12 @@ mod tests{
     #[test]
     fn test_step(){
         let a:Step = Step::Get("abc".to_owned());
+    }
+
+    #[test]
+    fn test_plan()-> Result<(), Box<dyn std::error::Error>>{
+        let query = crate::parse::parse_query("query")?;
+        let plan = Plan::from(&query);
+        Ok(())
     }
 }
--- rsliquer2/src/value.rs
+++ rsliquer2/src/value.rs
@@ -1,12 +1,13 @@
 use serde_json;
 
-use std::{result::Result, collections::BTreeMap};
+use std::{borrow::Cow, collections::BTreeMap, result::Result};
 
 use crate::error::Error;
 use std::convert::{TryFrom, TryInto};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-pub enum Value{
+#[serde(untagged)]
+pub enum Value {
     None,
     Bool(bool),
     I32(i32),
@@ -18,169 +19,265 @@ pub enum Value{
     Bytes(Vec<u8>),
 }
 
-trait ValueType{
+trait ValueInfo {
     /// String identifier of the state type
-    fn identifier(&self)->&'static str;
+    /// Several types can be linked to the same identifier.
+    /// The identifier must be cross-platform
+    fn identifier(&self) -> Cow<'static, str>;
+
+    /// String name of the stored type
+    /// The type_name is more detailed than identifier.
+    /// The identifier does not need to be cross-platform, it serves more for information and debugging
+    fn type_name(&self) -> Cow<'static, str>;
 
     /// Default file extension; determines the default data format
     /// Must be consistent with the default_mimetype.
-    fn default_extension(&self)->&'static str;
+    fn default_extension(&self) -> Cow<'static, str>;
 
     /// Default file name
-    fn default_filename(&self)->&'static str;
+    fn default_filename(&self) -> Cow<'static, str>;
 
     /// Default mime type - must be consistent with the default_extension
-    fn default_mimetype(&self)->&'static str;
+    fn default_mimetype(&self) -> Cow<'static, str>;
 }
 
-impl ValueType for Value{
-    fn identifier(&self)->&'static str{
-        match self{
-            Value::None => "none",
-            Value::Bool(_) => "bool",
-            Value::I32(_) => "i32",
-            Value::I64(_) => "i64",
-            Value::F64(_) => "f64",
-            Value::Text(_) => "text",
-            Value::Array(_) => "array",
-            Value::Object(_) => "object",
-            Value::Bytes(_) => "bytes",
+impl ValueInfo for Value {
+    fn identifier(&self) -> Cow<'static, str> {
+        match self {
+            Value::None => "none".into(),
+            Value::Bool(_) => "generic".into(),
+            Value::I32(_) => "generic".into(),
+            Value::I64(_) => "generic".into(),
+            Value::F64(_) => "generic".into(),
+            Value::Text(_) => "text".into(),
+            Value::Array(_) => "generic".into(),
+            Value::Object(_) => "dictionary".into(),
+            Value::Bytes(_) => "bytes".into(),
         }
     }
 
-    fn default_extension(&self)->&'static str{
-        match self{
-            Value::None => "json",
-            Value::Bool(_) => "json",
-            Value::I32(_) => "json",
-            Value::I64(_) => "json",
-            Value::F64(_) => "json",
-            Value::Text(_) => "txt",
-            Value::Array(_) => "json",
-            Value::Object(_) => "json",
-            Value::Bytes(_) => "b",
+    fn type_name(&self) -> Cow<'static, str> {
+        match self {
+            Value::None => "none".into(),
+            Value::Bool(_) => "bool".into(),
+            Value::I32(_) => "i32".into(),
+            Value::I64(_) => "i64".into(),
+            Value::F64(_) => "f64".into(),
+            Value::Text(_) => "text".into(),
+            Value::Array(_) => "array".into(),
+            Value::Object(_) => "object".into(),
+            Value::Bytes(_) => "bytes".into(),
         }
     }
 
-    fn default_filename(&self)->&'static str{
-        match self{
-            Value::None => "data.json",
-            Value::Bool(_) => "data.json",
-            Value::I32(_) => "data.json",
-            Value::I64(_) => "data.json",
-            Value::F64(_) => "data.json",
-            Value::Text(_) => "text.txt",
-            Value::Array(_) => "data.json",
-            Value::Object(_) => "data.json",
-            Value::Bytes(_) => "binary.b",
+    fn default_extension(&self) -> Cow<'static, str> {
+        match self {
+            Value::None => "json".into(),
+            Value::Bool(_) => "json".into(),
+            Value::I32(_) => "json".into(),
+            Value::I64(_) => "json".into(),
+            Value::F64(_) => "json".into(),
+            Value::Text(_) => "txt".into(),
+            Value::Array(_) => "json".into(),
+            Value::Object(_) => "json".into(),
+            Value::Bytes(_) => "b".into(),
         }
     }
 
-    fn default_mimetype(&self)->&'static str{
-        match self{
-            Value::None => "application/json",
-            Value::Bool(_) => "application/json",
-            Value::I32(_) => "application/json",
-            Value::I64(_) => "application/json",
-            Value::F64(_) => "application/json",
-            Value::Text(_) => "text/plain",
-            Value::Array(_) => "application/json",
-            Value::Object(_) => "application/json",
-            Value::Bytes(_) => "application/octet-stream",
+    fn default_filename(&self) -> Cow<'static, str> {
+        match self {
+            Value::None => "data.json".into(),
+            Value::Bool(_) => "data.json".into(),
+            Value::I32(_) => "data.json".into(),
+            Value::I64(_) => "data.json".into(),
+            Value::F64(_) => "data.json".into(),
+            Value::Text(_) => "text.txt".into(),
+            Value::Array(_) => "data.json".into(),
+            Value::Object(_) => "data.json".into(),
+            Value::Bytes(_) => "binary.b".into(),
+        }
+    }
+
+    fn default_mimetype(&self) -> Cow<'static, str> {
+        match self {
+            Value::None => "application/json".into(),
+            Value::Bool(_) => "application/json".into(),
+            Value::I32(_) => "application/json".into(),
+            Value::I64(_) => "application/json".into(),
+            Value::F64(_) => "application/json".into(),
+            Value::Text(_) => "text/plain".into(),
+            Value::Array(_) => "application/json".into(),
+            Value::Object(_) => "application/json".into(),
+            Value::Bytes(_) => "application/octet-stream".into(),
         }
     }
 }
 
-impl TryFrom<Value> for i32{
-    type Error=Error;
-    fn try_from(value: Value) -> Result<Self, Self::Error>{
-        match value{
+impl TryFrom<Value> for i32 {
+    type Error = Error;
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
             Value::I32(x) => Ok(x),
-            Value::I64(x) => i32::try_from(x).map_err(|e| Error::ConversionError{message:format!("I64 to i32 conversion error {e}")}),
-            _ => Err(Error::ConversionError{message:format!("Can't convert {} to i32", value.identifier())}),
+            Value::I64(x) => i32::try_from(x).map_err(|e| Error::ConversionError {
+                message: format!("I64 to i32 conversion error {e}"),
+            }),
+            _ => Err(Error::ConversionError {
+                message: format!("Can't convert {} to i32", value.type_name()),
+            }),
         }
     }
 }
 
-impl From<i32> for Value{
-    fn from(value: i32) -> Value{
+impl From<i32> for Value {
+    fn from(value: i32) -> Value {
         Value::I32(value)
     }
 }
 
-impl TryFrom<Value> for String{
-    type Error=Error;
-    fn try_from(value: Value) -> Result<Self, Self::Error>{
-        match value{
+impl TryFrom<Value> for String {
+    type Error = Error;
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
             Value::Text(x) => Ok(x),
-            Value::I32(x) => Ok(format!("{}",x)),
-            Value::I64(x) => Ok(format!("{}",x)),
-            Value::F64(x) => Ok(format!("{}",x)),           
-            _ => Err(Error::ConversionError{message:format!("Can't convert {} to string", value.identifier())}),
+            Value::I32(x) => Ok(format!("{}", x)),
+            Value::I64(x) => Ok(format!("{}", x)),
+            Value::F64(x) => Ok(format!("{}", x)),
+            _ => Err(Error::ConversionError {
+                message: format!("Can't convert {} to string", value.type_name()),
+            }),
         }
     }
 }
 
-impl From<String> for Value{
-    fn from(value: String) -> Value{
+impl From<String> for Value {
+    fn from(value: String) -> Value {
         Value::Text(value)
     }
 }
-impl From<&str> for Value{
-    fn from(value: &str) -> Value{
+impl From<&str> for Value {
+    fn from(value: &str) -> Value {
         Value::Text(value.to_owned())
     }
 }
 
-
-trait ValueSerializer where Self:Sized{
-    fn as_bytes(&self, format:&str)->Result<Vec<u8>, Error>;
-    fn from_bytes(b: &[u8], format:&str)->Result<Self, Error>;
+trait ValueSerializer
+where
+    Self: Sized,
+{
+    fn as_bytes(&self, format: &str) -> Result<Vec<u8>, Error>;
+    fn from_bytes(b: &[u8], format: &str) -> Result<Self, Error>;
 }
 
-impl ValueSerializer for Value{
-    fn as_bytes(&self, format:&str)->Result<Vec<u8>, Error>{
-        match format{
-            "json" => serde_json::to_vec(self).map_err(|e| Error::SerializationError{message:format!("JSON errror {}",e), format:format.to_owned()}),
-            _ => Err(Error::SerializationError{message:format!("Unsupported format {}",format), format:format.to_owned()})
+impl ValueSerializer for Value {
+    fn as_bytes(&self, format: &str) -> Result<Vec<u8>, Error> {
+        match format {
+            "json" => serde_json::to_vec(self).map_err(|e| Error::SerializationError {
+                message: format!("JSON error {}", e),
+                format: format.to_owned(),
+            }),
+            _ => Err(Error::SerializationError {
+                message: format!("Unsupported format {}", format),
+                format: format.to_owned(),
+            }),
         }
     }
-    fn from_bytes(b: &[u8], format:&str)->Result<Self, Error>{
-        match format{
-            "json" => serde_json::from_slice(b).map_err(|e| Error::SerializationError{message:format!("JSON errror {}",e), format:format.to_owned()}),
-            _ => Err(Error::SerializationError{message:format!("Unsupported format {}",format), format:format.to_owned()})
+    fn from_bytes(b: &[u8], format: &str) -> Result<Self, Error> {
+        match format {
+            "json" => serde_json::from_slice(b).map_err(|e| Error::SerializationError {
+                message: format!("JSON error {}", e),
+                format: format.to_owned(),
+            }),
+            _ => Err(Error::SerializationError {
+                message: format!("Unsupported format {}", format),
+                format: format.to_owned(),
+            }),
         }
     }
 }
-
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn test1() -> Result<(), Box<dyn std::error::Error>>{
+    fn test1() -> Result<(), Box<dyn std::error::Error>> {
         println!("Hello.");
         let v = Value::I32(123);
         let b = v.as_bytes("json")?;
         println!("Serialized    {:?}: {}", v, std::str::from_utf8(&b)?);
-        let w:Value = ValueSerializer::from_bytes(&b, "json")?;
+        let w: Value = ValueSerializer::from_bytes(&b, "json")?;
         println!("De-Serialized {:?}", w);
         Ok(())
-    }   
+    }
     #[test]
-    fn test_convert_int() -> Result<(), Box<dyn std::error::Error>>{
+    fn test_convert_int() -> Result<(), Box<dyn std::error::Error>> {
         let v = Value::I32(123);
-        let x:i32 = v.try_into()?;
-        assert_eq!(x,123);
+        let x: i32 = v.try_into()?;
+        assert_eq!(x, 123);
         Ok(())
-    }   
+    }
     #[test]
-    fn test_convert_text() -> Result<(), Box<dyn std::error::Error>>{
+    fn test_convert_text() -> Result<(), Box<dyn std::error::Error>> {
         let v = Value::from("abc");
-        assert_eq!(v,Value::Text("abc".to_owned()));
-        let x:String = v.try_into()?;
-        assert_eq!(x,"abc");
+        assert_eq!(v, Value::Text("abc".to_owned()));
+        let x: String = v.try_into()?;
+        assert_eq!(x, "abc");
         Ok(())
-    }   
+    }
+    #[test]
+    fn test_serde_to_json() -> Result<(), Box<dyn std::error::Error>> {
+        let v = Value::None;
+        let s = serde_json::to_string(&v)?;
+        assert_eq!(s, "null");
+        let v = Value::Bool(true);
+        let s = serde_json::to_string(&v)?;
+        assert_eq!(s, "true");
+        let v = Value::I32(123);
+        let s = serde_json::to_string(&v)?;
+        assert_eq!(s, "123");
+        let v = Value::I64(123);
+        let s = serde_json::to_string(&v)?;
+        assert_eq!(s, "123");
+        let v = Value::F64(123.456);
+        let s = serde_json::to_string(&v)?;
+        assert_eq!(s, "123.456");
+        let v = Value::from("abc");
+        let s = serde_json::to_string(&v)?;
+        assert_eq!(s, "\"abc\"");
+        let v = Value::Array(vec![Value::None, Value::Bool(false), Value::I32(123)]);
+        let s = serde_json::to_string(&v)?;
+        assert_eq!(s, "[null,false,123]");
+        let mut m = BTreeMap::new();
+        m.insert("test".to_owned(),Value::None);
+        m.insert("a".to_owned(),Value::I32(123));
+        let v = Value::Object(m);
+        let s = serde_json::to_string(&v)?;
+        assert_eq!(s, "{\"a\":123,\"test\":null}");
+        Ok(())
+    }
+    #[test]
+    fn test_serde_from_json() -> Result<(), Box<dyn std::error::Error>> {
+        let v:Value = serde_json::from_str("null")?;
+        assert_eq!(v, Value::None);
+        let v:Value = serde_json::from_str("true")?;
+        assert_eq!(v, Value::Bool(true));
+        let v:Value = serde_json::from_str("123")?;
+        assert_eq!(v, Value::I32(123));
+        let v:Value = serde_json::from_str("123456789123456789")?;
+        assert_eq!(v, Value::I64(123456789123456789));
+        let v:Value = serde_json::from_str("123.456")?;
+        assert_eq!(v, Value::F64(123.456));
+        let v:Value = serde_json::from_str("[null, true, 123]")?;
+        assert_eq!(v, Value::Array(vec![Value::None, Value::Bool(true), Value::I32(123)]));
+        let v:Value = serde_json::from_str("{\"a\":123,\"test\":null}")?;
+        if let Value::Object(x) = v{
+            assert_eq!(x.len(), 2);
+            assert_eq!(x["a"], Value::I32(123));
+            assert_eq!(x["test"], Value::None);
+        }
+        else{
+            assert!(false);
+        }
+        Ok(())
+    }
 }
