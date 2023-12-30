d82ccfdd0c768c135c05fbf45761dc236286cfb4
Orest Dubay <orest-d@users.noreply.github.com>
2023-12-30 21:19:57 +0100

    liquers python store

--- liquers-py/liquers-py/src/store.rs
+++ liquers-py/liquers-py/src/store.rs
@@ -64,5 +64,84 @@ impl Store {
         }
     }
 
+    /// Remove data and metadata associated with the key
+    fn remove(&mut self, key: &crate::parse::Key) -> PyResult<()> {
+        match self.0.remove(&key.0) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string())),
+        }
+    }
 
+    /// Remove directory.
+    /// The key must be a directory.
+    /// It depends on the underlying store whether the directory must be empty.    
+    fn removedir(&mut self, key: &crate::parse::Key) -> PyResult<()> {
+        match self.0.removedir(&key.0) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string())),
+        }
+    }
+    
+    /// Returns true if store contains the key.
+    fn contains(&self, key: &crate::parse::Key) -> bool {
+        self.0.contains(&key.0)
+    }
+    
+    /// Returns true if key points to a directory.
+    fn is_dir(&self, key: &crate::parse::Key) -> bool {
+        self.0.is_dir(&key.0)
+    }
+
+    /// List or iterator of all keys
+    fn keys(&self) -> PyResult<Vec<crate::parse::Key>> {
+        match self.0.keys() {
+            Ok(keys) => Ok(keys.into_iter().map(|k| crate::parse::Key(k)).collect()),
+            Err(e) => Err(PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string())),
+        }
+    }
+
+    /// Return names inside a directory specified by key.
+    /// To get a key, names need to be joined with the key (key/name).
+    /// Complete keys can be obtained with the listdir_keys method.
+    fn listdir(&self, key: &crate::parse::Key) -> PyResult<Vec<String>> {
+        match self.0.listdir(&key.0) {
+            Ok(names) => Ok(names),
+            Err(e) => Err(PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string())),
+        }
+    }
+
+    /// Return keys inside a directory specified by key.
+    /// Only keys present directly in the directory are returned,
+    /// subdirectories are not traversed.
+    fn listdir_keys(&self, key: &crate::parse::Key) -> PyResult<Vec<crate::parse::Key>> {
+        match self.0.listdir_keys(&key.0) {
+            Ok(keys) => Ok(keys.into_iter().map(|k| crate::parse::Key(k)).collect()),
+            Err(e) => Err(PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string())),
+        }
+    }
+
+    /// Return keys inside a directory specified by key.
+    /// Keys directly in the directory are returned,
+    /// as well as in all the subdirectories.
+    fn listdir_keys_deep(&self, key: &crate::parse::Key) -> PyResult<Vec<crate::parse::Key>> {
+        match self.0.listdir_keys_deep(&key.0) {
+            Ok(keys) => Ok(keys.into_iter().map(|k| crate::parse::Key(k)).collect()),
+            Err(e) => Err(PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string())),
+        }
+    }
+
+    /// Make a directory
+    fn makedir(&self, key: &crate::parse::Key) -> PyResult<()> {
+        match self.0.makedir(&key.0) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(PyErr::new::<pyo3::exceptions::PyException, _>(e.to_string())),
+        }
+    }
+
+    /// Returns true when this store supports the supplied key.
+    /// This allows layering Stores, e.g. by with_overlay, with_fallback
+    /// and store selectively certain data (keys) in certain stores.
+    fn is_supported(&self, key: &crate::parse::Key) -> bool {
+        self.0.is_supported(&key.0)
+    }
 }
