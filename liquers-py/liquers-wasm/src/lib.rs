dc507b8a0e2da1d919f857175423fae3a9bfbdf4
Orest Dubay <orest-d@users.noreply.github.com>
2024-01-05 23:14:00 +0100

    parsing test

--- liquers-py/liquers-wasm/src/lib.rs
+++ liquers-py/liquers-wasm/src/lib.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,4 +9,24 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn parse_query(query: &str) -> Result<JsValue, JsValue> {
+    match liquers_core::parse::parse_query(query) {
+        Ok(q) => {
+            serde_wasm_bindgen::to_value(&q).map_err(|e| JsValue::from_str(&e.to_string()))
+        }
+        Err(e) => Err(JsValue::from_str(&e.to_string())),
+    }
+}
+
+#[wasm_bindgen]
+pub fn parse_key(key: &str) -> Result<JsValue, JsValue> {
+    match liquers_core::parse::parse_key(key) {
+        Ok(k) => {
+            serde_wasm_bindgen::to_value(&k).map_err(|e| JsValue::from_str(&e.to_string()))
+        }
+        Err(e) => Err(JsValue::from_str(&e.to_string())),
+    }
 }
